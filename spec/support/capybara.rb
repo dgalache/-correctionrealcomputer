d712ea8d932a72ef97c35145ec1f0813165fc16a
dgalache <dangalaxe@gmail.com>
2015-12-19 18:44:57 -0200

    Reparando

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,41 +1,6 @@
-
-          require 'capybara/rspec'
-          require 'capybara/poltergeist'
-
-          Capybara.register_driver :poltergeist do |app|
-            Capybara::Poltergeist::Driver.new(app, js_errors: false, timeout: 5000)
-          end
-
-          Capybara.javascript_driver = :poltergeist
-
-
-        factory_girl.rb
-
-          RSpec.configure do |config|
-            config.include FactoryGirl::Syntax::Methods
-          end
-
-        database_cleaner.db
-
-          RSpec.configure do |config|
-            config.before(:suite) do
-              DatabaseCleaner.clean_with(:truncation)
-            end
-
-            config.before(:each) do
-              DatabaseCleaner.strategy = :transaction
-            end
-
-            config.before(:each, :js => true) do
-              DatabaseCleaner.strategy = :truncation
-            end
-
-            config.before(:each) do
-              DatabaseCleaner.start
-            end
-
-            config.append_after(:each) do
-              DatabaseCleaner.clean
-            end
-          end
-
+require 'capybara/rspec'
+	require 'capybara/poltergeist'
+		Capybara.register_driver :poltergeist do |app|
+		Capybara::Poltergeist::Driver.new(app, js_errors: false, timeout: 5000)
+	end
+		Capybara.javascript_driver = :poltergeist
